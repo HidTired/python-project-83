cf28035ed802172351a37227c0db441eca6ef007
HidTired <Dentanyabonya@mail.ru>
2025-12-21 04:59:12 +0300

    work

--- database.sql
+++ database.sql
@@ -1,20 +1,15 @@
-import sqlite3
-conn = sqlite3.connect('database.db')
-conn.execute('''CREATE TABLE IF NOT EXISTS urls (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL,
+CREATE TABLE urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR UNIQUE NOT NULL,
     created_at DATE NOT NULL
-)''')
-conn.execute('''CREATE TABLE IF NOT EXISTS url_checks (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    url_id INTEGER,
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls(id),
     status_code INTEGER,
     h1 TEXT,
     title TEXT,
     description TEXT,
-    created_at DATE NOT NULL,
-    FOREIGN KEY(url_id) REFERENCES urls(id)
-)''')
-conn.commit()
-conn.close()
-exit()
+    created_at DATE NOT NULL
+);
